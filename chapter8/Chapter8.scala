3c7af7c2cab3f8a411f8c1790513ec1e1d81ed54
Roman Valiusenko <roman.valiusenko@gmail.com>
2019-07-08 22:09:04 +0100

    Chapter 8

--- chapter8/Chapter8.scala
+++ chapter8/Chapter8.scala
@@ -1,4 +1,5 @@
 import Chapter6._
+import java.lang.System._
 
 object Chapter8 extends App {
 
@@ -13,7 +14,11 @@ object Chapter8 extends App {
   }
   */
 
-  case class Gen[A](sample: State[RNG, A]) {
+  case class SGen[+A](forSize: Int => Gen[A]) {
+
+  }
+
+  case class Gen[+A](sample: State[RNG, A]) {
     // 8.4
     def choose(start: Int, stopExclusive: Int): Gen[Int] = {
       Gen[Int](State(RNG.nonNegativeLessThan(stopExclusive - start)).map(v => v + start))
@@ -44,12 +49,27 @@ object Chapter8 extends App {
       Gen(State(RNG.double).flatMap(x => if (x <= p) g1._1.sample else g2._1.sample))
     }
 
-    // 8.9
+    // 8.10
+    def unsized: SGen[A] = SGen {
+      s => this
+    }
+
+    // 8.11
+    // 8.12
+    def listOf[A](g: Gen[A]): SGen[List[A]] = SGen {
+      s => listOfN(s, g)
+    }
+
+    // 8.13
+    def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen {
+      s => g.listOfN(if (s > 0) s else 1)
+    }
   }
 
   type TestCases = Int
   type FailedCase = String
   type SuccessCount = Int
+  type MaxSize = Int
 
   sealed trait Result {
     def isFalsified: Boolean
@@ -63,24 +83,64 @@ object Chapter8 extends App {
     def isFalsified = true
   }
 
-  case class Prop(run: (TestCases, RNG) => Result) {
+  case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
     def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
-      (n, rng) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
-        case (a, i) => try {
-          if (f(a)) Passed else Falsified(a.toString, i)
-        } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
-      }.find(_.isFalsified).getOrElse(Passed)
+      (_, n, rng) =>
+        randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+          case (a, i) => try {
+            if (f(a)) Passed else Falsified(a.toString, i)
+          } catch {
+            case e: Exception => Falsified(buildMsg(a, e), i)
+          }
+        }.find(_.isFalsified).getOrElse(Passed)
+    }
+
+    //    def forAll[A](g: SGen[A])(f: A => Boolean): Prop =
+    //      forAll(g(_))(f)
+
+    def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop {
+      (max, n, rng) =>
+        val casesPerSize = (n + (max - 1)) / max
+        val props: Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+        val prop: Prop = props.map(p => Prop { (max, _, rng) =>
+          p.run(max, casesPerSize, rng)
+        }).toList.reduce(_ && _)
+        prop.run(max, n, rng)
     }
 
     def randomStream[A](g: Gen[A])(rng: RNG): Stream[A] =
       Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
     def buildMsg[A](s: A, e: Exception): String =
       s"test case: $s\n" +
-      s"generated an exception: ${e.getMessage}\n" +
-      s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+        s"generated an exception: ${e.getMessage}\n" +
+        s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+
+    // 8.9
+    def &&(p: Prop): Prop = Prop {
+      (max, n, rng) => {
+        val resLeft = this.run(max, n, rng)
+        if (resLeft.isFalsified) resLeft else p.run(max, n, rng)
+      }
+    }
+
+    def ||(p: Prop): Prop = Prop {
+      (max, n, rng) => {
+        val resLeft = this.run(max, n, rng)
+        if (resLeft.isFalsified) p.run(max, n, rng) else resLeft
+      }
+    }
   }
 
-
+  object Prop {
+    def run(p: Prop, maxSize: Int = 100, testCases: TestCases = 100, rng: RNG = RNG.Simple(System.currentTimeMillis)): Unit =
+      p.run(maxSize, testCases, rng) match {
+        case Falsified(msg, n) =>
+          println(s"! Falsified after $n passed testes:\n $msg")
+        case Passed =>
+          println(s"+ OK, passed $testCases tests.")
+      }
+  }
 
   println("hello")
 }
