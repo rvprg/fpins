013790aa51ead7b0e7a87bc77df50b6b706375be
Roman Valiusenko <roman.valiusenko@gmail.com>
2019-08-28 21:45:56 +0100

    Chapter 12

--- chapter12/Chapter12.scala
+++ chapter12/Chapter12.scala
@@ -45,6 +45,7 @@ object Chapter12 extends App {
 
     def factor[A, B](fa: F[A], fb: F[B]): F[(A, B)] = ???
 
+    // 12.8
     def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = ???
 
     def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = ???
@@ -88,14 +89,6 @@ object Chapter12 extends App {
     Monad[({type f[x] = F[N[x]]})#f] = ???
   }
 
-  sealed trait Validation[+E, +A]
-
-  case class Failure[E](head: E, tail: Vector[E])
-    extends Validation[E, Nothing]
-
-  case class Success[A](a: A) extends Validation[Nothing, A]
-
-
   object Applicative {
 
     val streamApplicative = new Applicative[Stream] {
@@ -113,7 +106,24 @@ object Chapter12 extends App {
     // elements as the input list, where each element is the original stream converted
     // to a list.
 
-    def validationApplicative[E]: Applicative[({type f[x] = Validation[E, x]})#f] = ???
+    sealed trait Validation[+E, +A]
+
+    case class Failure[E](head: E, tail: Vector[E])
+      extends Validation[E, Nothing]
+
+    case class Success[A](a: A) extends Validation[Nothing, A]
+
+    // 12.6
+    def validationApplicative[E] = new Applicative[({type f[x] = Validation[E, x]})#f] {
+      override def unit[A](a: => A): Validation[E, A] = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa, fb) match {
+        case (Success(a), Success(b)) => unit(f(a, b))
+        case (Failure(h, t), Success(_)) => Failure(h, t)
+        case (Success(_), Failure(h, t)) => Failure(h, t)
+        case (Failure(h1, t1), Failure(h2, t2)) => Failure(h1, Vector(h2).appendedAll(t1).appended(t2))
+      }
+    }
 
     type Const[A, B] = A
 
